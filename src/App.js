67bb64fb4a8f8aa6e7449e1088f877e231a48219
rohansinghwow <rohansinghwow@gmail.com>
2021-12-19 16:48:33 +0530

    Setting up project

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./App.css";
+import img from "./image-equilibrium.jpg";
+
+export default function App() {
+  let g1 = "hjshjhf";
+
+  function sjhdjdhj() {
+    <>
+      <h1>asddac</h1>
+      <p></p>
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </>;
+  }
+  return (
+    <>
+      <h1>Shit on me!</h1>
+      <button>Heheh</button>
+
+      <button></button>
+    </>
+  );
+}
